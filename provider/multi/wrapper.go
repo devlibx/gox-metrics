787ce3e8dc4230dd0fbfe48a67db6a6255b39bc9
Harish Bohara <you@example.com>
2021-05-27 00:39:21 +0530

    added support for prometheus and composit

---
+++ provider/multi/wrapper.go
@@ -0,0 +1,184 @@
+package multi
+
+import (
+	"github.com/devlibx/gox-base/errors"
+	"github.com/devlibx/gox-base/metrics"
+	"github.com/devlibx/gox-metrics/provider/prometheus"
+	"github.com/devlibx/gox-metrics/provider/statsd"
+	"io"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var postfix = ""
+
+func NewRootScope(config metrics.Config) (metrics.ClosableScope, error) {
+	s, err := statsd.NewRootScope(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to build statsd root scope")
+	}
+
+	p, err := prometheus.NewRootScope(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to build prometheus root scope")
+	}
+
+	m := &multiMetrics{
+		statsdMetrics:              s,
+		prometheusMetrics:          p,
+		statsdMetricsCloseOnce:     sync.Once{},
+		prometheusMetricsCloseOnce: sync.Once{},
+	}
+
+	return m, nil
+}
+
+func (s *multiMetrics) HTTPHandler() http.Handler {
+	reporter := s.prometheusMetrics.(metrics.Reporter)
+	return reporter.HTTPHandler()
+}
+
+type multiMetrics struct {
+	statsdMetrics              metrics.Scope
+	prometheusMetrics          metrics.Scope
+	statsdMetricsCloseOnce     sync.Once
+	prometheusMetricsCloseOnce sync.Once
+	statsdMetricsCloser        io.Closer
+	prometheusMetricsCloser    io.Closer
+}
+
+func (m *multiMetrics) Counter(name string) metrics.Counter {
+	c := compositeCounter{
+		statsd:     m.statsdMetrics.Counter(name),
+		prometheus: m.prometheusMetrics.Counter(name + postfix),
+	}
+	return &c
+}
+
+func (m *multiMetrics) Gauge(name string) metrics.Gauge {
+	c := compositeGauge{
+		statsd:     m.statsdMetrics.Gauge(name),
+		prometheus: m.prometheusMetrics.Gauge(name + postfix),
+	}
+	return &c
+}
+
+func (m *multiMetrics) Timer(name string) metrics.Timer {
+	c := compositeTimer{
+		statsd:     m.statsdMetrics.Timer(name),
+		prometheus: m.prometheusMetrics.Timer(name+postfix),
+	}
+	return &c
+}
+
+func (m *multiMetrics) Histogram(name string, buckets metrics.Buckets) metrics.Histogram {
+	c := compositeHistogram{
+		statsd:     m.statsdMetrics.Histogram(name, buckets),
+		prometheus: m.prometheusMetrics.Histogram(name+postfix, buckets),
+	}
+	return &c
+}
+
+func (m *multiMetrics) Tagged(tags map[string]string) metrics.Scope {
+	return &multiMetrics{
+		statsdMetrics:     m.statsdMetrics.Tagged(tags),
+		prometheusMetrics: m.prometheusMetrics.Tagged(tags),
+	}
+}
+
+func (m *multiMetrics) SubScope(name string) metrics.Scope {
+	return &multiMetrics{
+		statsdMetrics:     m.statsdMetrics.SubScope(name),
+		prometheusMetrics: m.prometheusMetrics.SubScope(name+postfix),
+	}
+}
+
+func (m *multiMetrics) Capabilities() metrics.Capabilities {
+	return m.prometheusMetrics.Capabilities()
+}
+
+func (m *multiMetrics) Stop() error {
+	m.statsdMetricsCloseOnce.Do(func() {
+		closable := m.statsdMetricsCloser.(metrics.ClosableScope)
+		_ = closable.Stop()
+	})
+	m.prometheusMetricsCloseOnce.Do(func() {
+		closable := m.prometheusMetrics.(metrics.ClosableScope)
+		_ = closable.Stop()
+	})
+	return nil
+}
+
+type compositeCounter struct {
+	statsd     metrics.Counter
+	prometheus metrics.Counter
+}
+
+func (c *compositeCounter) Inc(delta int64) {
+	c.statsd.Inc(delta)
+	c.prometheus.Inc(delta)
+}
+
+type compositeGauge struct {
+	statsd     metrics.Gauge
+	prometheus metrics.Gauge
+}
+
+func (c *compositeGauge) Update(value float64) {
+	c.statsd.Update(value)
+	c.prometheus.Update(value)
+}
+
+type compositeTimer struct {
+	statsd     metrics.Timer
+	prometheus metrics.Timer
+}
+
+func (c *compositeTimer) Record(value time.Duration) {
+	c.statsd.Record(value)
+	c.prometheus.Record(value)
+}
+
+func (c *compositeTimer) Start() metrics.Stopwatch {
+	sw1 := c.statsd.Start()
+	sw2 := c.prometheus.Start()
+	return &compositeStopwatch{
+		statsd:     sw1,
+		prometheus: sw2,
+	}
+}
+
+type compositeStopwatch struct {
+	statsd     metrics.Stopwatch
+	prometheus metrics.Stopwatch
+}
+
+func (c *compositeStopwatch) Stop() {
+	c.statsd.Stop()
+	c.prometheus.Stop()
+}
+
+type compositeHistogram struct {
+	statsd     metrics.Histogram
+	prometheus metrics.Histogram
+}
+
+func (c *compositeHistogram) RecordValue(value float64) {
+	c.statsd.RecordValue(value)
+	c.prometheus.RecordValue(value)
+}
+
+func (c *compositeHistogram) RecordDuration(value time.Duration) {
+	c.statsd.RecordDuration(value)
+	c.prometheus.RecordDuration(value)
+}
+
+func (c *compositeHistogram) Start() metrics.Stopwatch {
+	sw1 := c.statsd.Start()
+	sw2 := c.prometheus.Start()
+	return &compositeStopwatch{
+		statsd:     sw1,
+		prometheus: sw2,
+	}
+}
---
+++ provider/prometheus/wrapper.go
@@ -0,0 +1,107 @@
+package prometheus
+
+import (
+	"github.com/devlibx/gox-base/metrics"
+	"github.com/uber-go/tally"
+	promreporter "github.com/uber-go/tally/prometheus"
+	"io"
+	"net/http"
+	"sync"
+	"time"
+)
+
+// Wrapper of tally timer
+type timer struct {
+	timer tally.Timer
+}
+
+func (t *timer) Record(value time.Duration) {
+	t.timer.Record(value)
+}
+
+func (t *timer) Start() metrics.Stopwatch {
+	return t.timer.Start()
+}
+
+// Wrapper of tally histogram
+type histogram struct {
+	histogram tally.Histogram
+}
+
+func (h *histogram) RecordValue(value float64) {
+	h.histogram.RecordValue(value)
+}
+
+func (h *histogram) RecordDuration(value time.Duration) {
+	h.histogram.RecordDuration(value)
+}
+
+func (h *histogram) Start() metrics.Stopwatch {
+	return h.histogram.Start()
+}
+
+// Wrapper of tally scope class
+type prometheusMetrics struct {
+	scope     tally.Scope
+	closer    io.Closer
+	closeOnce sync.Once
+	reporter metrics.Reporter
+}
+
+func (s *prometheusMetrics) Counter(name string) metrics.Counter {
+	return s.scope.Counter(name)
+}
+
+func (s *prometheusMetrics) Gauge(name string) metrics.Gauge {
+	return s.scope.Gauge(name)
+}
+
+func (s *prometheusMetrics) Timer(name string) metrics.Timer {
+	return &timer{timer: s.scope.Timer(name)}
+}
+
+func (s *prometheusMetrics) Histogram(name string, buckets metrics.Buckets) metrics.Histogram {
+	return &histogram{histogram: s.scope.Histogram(name, buckets)}
+}
+
+func (s *prometheusMetrics) Tagged(tags map[string]string) metrics.Scope {
+	return &prometheusMetrics{scope: s.scope.Tagged(tags)}
+}
+
+func (s *prometheusMetrics) SubScope(name string) metrics.Scope {
+	return &prometheusMetrics{scope: s.scope.SubScope(name)}
+}
+
+func (s *prometheusMetrics) Capabilities() metrics.Capabilities {
+	return s.scope.Capabilities()
+}
+
+func (s *prometheusMetrics) Stop() error {
+	s.closeOnce.Do(func() {
+		_ = s.closer.Close()
+	})
+	return nil
+}
+
+func (s *prometheusMetrics) HTTPHandler() http.Handler {
+	return s.reporter.HTTPHandler()
+}
+
+
+func NewRootScope(config metrics.Config) (metrics.ClosableScope, error) {
+
+	reporter := promreporter.NewReporter(promreporter.Options{})
+
+	// Create tally specific scope object to use
+	scope, closer := tally.NewRootScope(
+		tally.ScopeOptions{
+			Prefix:         config.Prefix,
+			Tags:           map[string]string{},
+			CachedReporter: reporter,
+			Separator:      promreporter.DefaultSeparator,
+		},
+		time.Duration(config.ReportingIntervalMs)*time.Millisecond,
+	)
+
+	return &prometheusMetrics{scope: scope, closer: closer, closeOnce: sync.Once{}, reporter: reporter}, nil
+}
